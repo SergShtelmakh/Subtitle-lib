66ced24edfef93d5942063845f6e63fd0f8b14ef
S_E_R_G <sh.serg1993@gmail.com>
2017-03-19 23:17:14 +0200

    Updated script

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -2,16 +2,15 @@
 set -ev
 
 # Setup display
-export DISPLAY=:99.0
-sh -e /etc/init.d/xvfb start
+#export DISPLAY=:99.0
+#sh -e /etc/init.d/xvfb start
 
 # Install Ubuntu packages
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ntadej/tano ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 
 # Install Qt
-sudo apt-get install qtbase5-dev qtdeclarative5-dev qtdeclarative5-dev-tools qtdeclarative5-qtquick2-plugin qtdeclarative5-test-plugin -y
-
+sudo apt-get install qtbase5-dev qtdeclarative5-dev qtdeclarative5-dev-tools qtdeclarative5-qtquick2-plugin qtdeclarative5-test-plugin
 sudo apt-get install g++-4.8
 export CXX="g++-4.8"
 
