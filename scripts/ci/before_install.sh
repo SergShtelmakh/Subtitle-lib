2956321678c7a6d64fcfb6851cf4307e53c9b38e
S_E_R_G <sh.serg1993@gmail.com>
2017-03-19 22:15:49 +0200

    Setup compiler for Travis CI

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -9,6 +9,11 @@ sh -e /etc/init.d/xvfb start
 sudo add-apt-repository ppa:ntadej/tano -y
 sudo apt-get update -qq
 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -qq
+if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
+if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
+
 # Install Qt
 if [ $QT_VERSION -eq 5 ]
 then
