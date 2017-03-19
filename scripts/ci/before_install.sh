26b888692c0b1bf59e8ce8e823696dc8bc7d344d
S_E_R_G <sh.serg1993@gmail.com>
2017-03-19 23:00:19 +0200

    Updated scripts

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -10,12 +10,10 @@ sudo add-apt-repository ppa:ntadej/tano -y
 sudo apt-get update -qq
 
 # Install Qt
-if [ $QT_VERSION -eq 5 ]
-then
-    sudo apt-get install qtbase5-dev qtdeclarative5-dev qtdeclarative5-dev-tools qtdeclarative5-qtquick2-plugin qtdeclarative5-test-plugin -y
-else
-    sudo apt-get install libqt4-dev -y
-fi
+sudo apt-get install qtbase5-dev qtdeclarative5-dev qtdeclarative5-dev-tools qtdeclarative5-qtquick2-plugin qtdeclarative5-test-plugin -y
+
+sudo apt-get install -qq g++-4.8
+export CXX="g++-4.8"
 
 # Prepare build environment
 mkdir build
